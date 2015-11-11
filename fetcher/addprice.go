71b2a2f0992a7b1e6d546454ba2c912784f9cf88
Anonymous <spogboy@gmail.com>
2015-11-11 09:13:29 +0000

    Fixed the empty entries when the start of the month occurred on a weekend.

--- fetcher/addprice.go
+++ fetcher/addprice.go
@@ -1,13 +1,19 @@
 package fetcher
 
 import (
+   "time"
 	"github.com/Fepelus/goldhouse/entities"
 )
 
 func (this goldFetcher) addGoldToPrices(golddata GoldData) entities.DatedPrices {
   mappedGold := convertGolddataToMap(golddata)
   for i, _ := range this.datedprices {
-    datestring := this.datedprices[i].Date.Format("2006-01-02")
+    thisdate := this.datedprices[i].Date
+    datestring := thisdate.Format("2006-01-02")
+    for price := mappedGold[datestring]; price == 0; price = mappedGold[datestring] {
+      thisdate = thisdate.Add(-24 * time.Hour)
+      datestring = thisdate.Format("2006-01-02")
+    } 
     this.datedprices[i].GoldPrice = mappedGold[datestring]
   }
 	return this.datedprices
